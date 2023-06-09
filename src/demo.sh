ed29e328abaeb65dd03415b5917c7f35d94a2e45
Data Team <patrick.ng@networld.hk>
2023-06-09 11:13:26 +0800

    Bug fix

--- src/demo.sh
+++ src/demo.sh
@@ -7,7 +7,6 @@ model='NAML'
 use_category=True
 use_subcategory=False
 enable_gpu=False
-prepare=False
 
 if [ ${mode} == train ]
 then
@@ -15,19 +14,19 @@ then
     batch_size=32
     lr=0.0003
     user_log_mask=False
-    prepare=True
+    prepare=False
     python -u main.py --mode train --model_dir ${model_dir} --batch_size ${batch_size} --epochs ${epochs} --model ${model} \
     --lr ${lr} --user_log_mask ${user_log_mask} --prepare ${prepare} --nGPU ${nGPU} --enable_gpu ${enable_gpu} \
-    --use_category ${use_category} --use_subcategory ${use_subcategory} --prepare ${prepare}
+    --use_category ${use_category} --use_subcategory ${use_subcategory}
 elif [ ${mode} == test ]
 then
     user_log_mask=True
     batch_size=128
     load_ckpt_name=$2
-    prepare=True
+    prepare=False
     python -u main.py --mode test --model_dir ${model_dir} --batch_size ${batch_size} --user_log_mask ${user_log_mask} \
     --load_ckpt_name ${load_ckpt_name} --model ${model} --prepare ${prepare} --nGPU ${nGPU} --enable_gpu ${enable_gpu} \
-    --use_category ${use_category} --use_subcategory ${use_subcategory} --prepare ${prepare}
+    --use_category ${use_category} --use_subcategory ${use_subcategory}
 else
     echo "Please select train or test mode."
 fi
--- src/demo_colab.sh
+++ src/demo_colab.sh
@@ -7,7 +7,6 @@ model='NAML'
 use_category=True
 use_subcategory=False
 enable_gpu=False
-prepare=False
 
 if [ ${mode} == train ]
 then
@@ -18,7 +17,7 @@ then
     prepare=True
     python -u main.py --mode train --model_dir ${model_dir} --batch_size ${batch_size} --epochs ${epochs} --model ${model} \
     --lr ${lr} --user_log_mask ${user_log_mask} --prepare ${prepare} --nGPU ${nGPU} --enable_gpu ${enable_gpu} \
-    --use_category ${use_category} --use_subcategory ${use_subcategory} --prepare ${prepare}
+    --use_category ${use_category} --use_subcategory ${use_subcategory}
 elif [ ${mode} == test ]
 then
     user_log_mask=True
@@ -27,7 +26,7 @@ then
     prepare=True
     python -u main.py --mode test --model_dir ${model_dir} --batch_size ${batch_size} --user_log_mask ${user_log_mask} \
     --load_ckpt_name ${load_ckpt_name} --model ${model} --prepare ${prepare} --nGPU ${nGPU} --enable_gpu ${enable_gpu} \
-    --use_category ${use_category} --use_subcategory ${use_subcategory} --prepare ${prepare}
+    --use_category ${use_category} --use_subcategory ${use_subcategory}
 else
     echo "Please select train or test mode."
 fi
